4cf4cc6c10beedf3509d981202d986f8f4b26717
ShadeSnuff <sabanovic.kenan@gmail.com>
2016-04-22 20:08:52 +0200

    Added files via upload

---
+++ .provision/bootstrap.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# install nginx
+sudo apt-get -y install nginx
+sudo service nginx start
+
+# set up nginx server
+sudo cp /vagrant/.provision/nginx/nginx.conf /etc/nginx/sites-available/site.conf
+sudo chmod 644 /etc/nginx/sites-available/site.conf
+sudo ln -s /etc/nginx/sites-available/site.conf /etc/nginx/sites-enabled/site.conf
+sudo service nginx restart
+
+# clean /var/www
+sudo rm -Rf /var/www
+
+# symlink /var/www => /vagrant
+ln -s /vagrant /var/www
+
+
+# install debconf/mysql
+apt-get install debconf-utils -y > /dev/null
+debconf-set-selections <<< "mysql-server mysql-server/root_password password 1234"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 1234"
+apt-get install mysql-server -y > /dev/null
